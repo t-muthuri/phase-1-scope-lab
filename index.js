c9ba13fcd25638c65dba3adfc4fe2ee0e2509816
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-25 21:20:41 +0300

    Commit assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function name(){
+    return `${customerName}`;
+}
+
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer (){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFave = 'Creek'
+function changeLeastFavoriteCustomer (){
+    return leastFave = 'Rumble'
+}
